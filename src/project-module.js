26fb65a6cab344acfc91a53d3c73cd9464fef631
Joe Chek <jmchek@gmail.com>
2024-03-21 13:31:20 -0700

    remove duplicate projects display in card and replace with updated todos data by rebuilding the display (refactored)

--- src/project-module.js
+++ src/project-module.js
@@ -39,17 +39,109 @@ export default function projectsModule(project, todos) {
         //CARD UPDATER
         let projCards = document.querySelectorAll('.card-container');
         let dupeFound = false;
+        let dupe;
+        let dupeIndex;
 
-        projCards.forEach(card => {
+        projCards.forEach((card, indx) => {
             if (card.children[0].children[0].textContent == project) {
                 dupeFound = true;
+                dupe = projCards[indx];
+                dupeIndex = indx;
             }
         });
 
         if (dupeFound){
-            console.log("Dupe found!");
+            //update the card if dupe found
+            dupe.children[0].children[1].remove();
+
+            let prjCard = Array.from(document.querySelectorAll('.project-card'));
+            let latestPrjCard = [prjCard[dupeIndex]];
+
+            latestPrjCard[0].insertBefore(document.createElement('div'), latestPrjCard[0].children[1]).setAttribute('class', 'projects-display');
+
+            latestPrjCard[0].children[1].appendChild(document.createElement('ul')).setAttribute('class', 'projects-display-ul');
+
+            Object.entries(todos).forEach(([key, value]) => {
+
+                latestPrjCard[0].children[1].children[0].appendChild(document.createElement('li')).innerText = `${JSON.stringify(value.title).replaceAll('"', '')}, ${JSON.stringify(value.dueDate).replaceAll('"', '')}`;
+    
+                latestPrjCard[0].children[1].children[0].children[key].classList.add('todos-li');
+    
+                latestPrjCard[0].children[1].children[0].children[key].appendChild(document.createElement('input')).setAttribute('class', 'edit-btn-todos-li');
+    
+                //edit button
+                latestPrjCard[0].children[1].children[0].children[key].children[0].addEventListener('click', editEle => {
+                    if (!document.querySelector('.edit-btn-div-form')) {
+                        projContGrbbr.appendChild(document.createElement('div')).setAttribute('class', 'edit-btn-div-form');
+    
+                        document.querySelector('.edit-btn-div-form').appendChild(document.createElement('h3')).setAttribute('class', 'edit-btn-div-form-header');
+                        document.querySelector('.edit-btn-div-form-header').innerText = "Edit Todo";
+    
+                        document.querySelector('.edit-btn-div-form').appendChild(document.createElement('input')).setAttribute('class', 'edit-btn-div-form-btn');
+                        document.querySelector('.edit-btn-div-form-btn').setAttribute('type', 'button');
+                        document.querySelector('.edit-btn-div-form-btn').value = "X";
+    
+                        formMaker(document.querySelector('.edit-btn-div-form'),todos[key]);
+        
+                        document.querySelector('.edit-btn-div-form-btn').addEventListener('click', editEleFocus => {
+                            document.querySelector('.edit-btn-div-form').remove();
+                        });
+                    }
+                    
+                });
+    
+                latestPrjCard[0].children[1].children[0].children[key].appendChild(document.createElement('input')).setAttribute('class', 'del-btn-todos-li');
+    
+                // latestPrjCard[0].children[1].children[0].children[key].children[1].appendChild(document.createElement('img')).src = DelButton;
+    
+                latestPrjCard[0].children[1].children[0].children[key].children[0].setAttribute('type', 'button');
+    
+                latestPrjCard[0].children[1].children[0].children[key].children[1].setAttribute('type', 'button');
+                
+                //expanded view
+                latestPrjCard[0].children[1].children[0].children[key].addEventListener('click', x => {
+                    if (x.target.tagName == "LI") {
+                        let newItem = document.createElement('div');
+                        let oldItem = latestPrjCard[0].children[1].children[0].children[key];
+                        
+                        newItem.setAttribute('class', 'expanded-view');
+        
+                        Object.entries(todos[key]).forEach(([newItemKey, newItemValue]) => {
+        
+                            if(newItemKey != "project" && typeof(newItemValue) == "string"){
+                                let paraChild = document.createElement('p');
+                                newItem.appendChild(paraChild);
+                                paraChild.setAttribute('class', 'expanded-view-paras');
+                                paraChild.innerText = JSON.stringify(newItemKey).replaceAll('"', '').charAt(0).toUpperCase() + JSON.stringify(newItemKey).replaceAll('"', '').slice(1) + ": " + JSON.stringify(newItemValue).replaceAll('"', '');
+                            } else if (newItemKey != "project") {
+                                let paraChild = document.createElement('p');
+                                newItem.appendChild(paraChild);
+                                paraChild.setAttribute('class', 'expanded-view-paras');
+                                paraChild.innerText = JSON.stringify(newItemKey).replaceAll('"', '').charAt(0).toUpperCase() + JSON.stringify(newItemKey).replaceAll('"', '').slice(1) + ": ";
+                            }
+                        });
+        
+                        newItem.addEventListener('click', y => {
+                            if(latestPrjCard[0].children[1].children[0].children[key].classList.contains('expanded-view')) {
+                                latestPrjCard[0].children[1].children[0].replaceChild(oldItem, latestPrjCard[0].children[1].children[0].children[key]);
+                            } else {
+                                latestPrjCard[0].children[1].children[0].replaceChild(newItem, latestPrjCard[0].children[1].children[0].children[key]);
+                            }
+                        });
+        
+                        if(latestPrjCard[0].children[1].children[0].children[key].classList.contains('expanded-view')) {
+                            latestPrjCard[0].children[1].children[0].replaceChild(oldItem, latestPrjCard[0].children[1].children[0].children[key]);
+                        } else {
+                            latestPrjCard[0].children[1].children[0].replaceChild(newItem, latestPrjCard[0].children[1].children[0].children[key]);
+                        }
+                    }
+                    
+                });
+            });
+
         } else {
-            projContGrbbr.appendChild(document.createElement('div')).setAttribute('class', 'card-container');
+        //CREATE A NEW CARD FOR PROJECT
+        projContGrbbr.appendChild(document.createElement('div')).setAttribute('class', 'card-container');
 
         let cardCntr = Array.from(document.querySelectorAll('.card-container'));
 
